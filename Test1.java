4ff80fd9f5ee1c5efd07516486c6d44a76ff7053
oofidc <70336313+oofidc@users.noreply.github.com>
2025-02-21 12:52:44 -0600

    Edit Test1.java, Create CompileAndRun Part of Application

--- Test1.java
+++ Test1.java
@@ -1,3 +1,8 @@
+
 public class Test1 {
-    
+    public static void main(String[] args){
+        for(int i = 0; i< 20; i++){
+            System.out.println("BABY, BABY, BABY, BABY -- MA LO TO RI E JE GBESE");
+        }
+    }
 }
